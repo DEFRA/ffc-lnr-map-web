c8b1aac3d186e979f4a7ae361f12522d0a9a882c
steve-dickinson <steve.dickinson@defra.gov.uk>
2022-10-11 14:07:26 +0100

    Add select

--- app/frontend/src/js/map-interaction.js
+++ app/frontend/src/js/map-interaction.js
@@ -1,16 +1,18 @@
-import { Draw, Modify, Snap } from 'ol/interaction'
+import { Select, Draw, Modify, Snap } from 'ol/interaction'
 import { landParcelStyles } from './styles/map-styles'
 import { drawStyles } from './styles/map-draw-styles'
 
 let draw
 let snap
+let modify
+let select
 
 const addInteraction = (map, source, drawSource, freehand, tracing, type = 'Polygon') => {
-  const modify = new Modify({ source: source.getSource() })
-  map.addInteraction(modify)
+  // const modify = new Modify({ source: drawSource.getSource() })
+  // map.addInteraction(modify)
 
   draw = new Draw({
-    source: source.getSource(),
+    source: drawSource.getSource(),
     type,
     freehand,
     trace: tracing,
@@ -26,20 +28,50 @@ const addInteraction = (map, source, drawSource, freehand, tracing, type = 'Poly
   draw.on('drawstart', (e) => {
     e.feature.setStyle(landParcelStyles.PolygonNew)
   })
+
+  draw.on('drawend', (e) => {
+    document.getElementById('draw').checked = false
+    map.removeInteraction(draw)
+    map.removeInteraction(snap)
+  })
 }
 
 const selectInteraction = (map, source, drawSource) => {
-  // const selectDraw = document.getElementById('draw-select')
+  const selectModify = document.getElementById('select-modify')
+  const selectDraw = document.getElementById('draw')
   const selectFreehand = document.getElementById('draw-freehand')
   // const selectTracing = document.getElementById('draw-tracing')
 
   const onChange = () => {
     map.removeInteraction(draw)
     map.removeInteraction(snap)
-    addInteraction(map, source, drawSource, selectFreehand.checked, false)
+    map.removeInteraction(modify)
+    map.removeInteraction(select)
+
+    if (selectModify.checked) {
+
+      select = new Select({
+        wrapX: false
+      })
+
+      select.on('select', (e) => {
+        console.log(e)
+      })
+
+      console.log('select', select)
+
+      modify = new Modify({ source: drawSource, features: select.getFeatures() })
+      map.addInteraction(select)
+      map.addInteraction(modify)
+    }
+
+    if(selectDraw.checked) {
+      addInteraction(map, source, drawSource, selectFreehand.checked, false)
+    }
   }
 
-  // selectDraw.addEventListener('change', onChange)
+  selectModify.addEventListener('change', onChange)
+  selectDraw.addEventListener('change', onChange)
   selectFreehand.addEventListener('change', onChange)
   // selectTracing.addEventListener('change', onChange)
 
